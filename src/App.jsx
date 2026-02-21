f34dd317694b5d570f1587199f7f8b17c392ca53
SammySparrow <samstevens.dev@gmail.com>
2026-02-22 08:55:09 +1100

    Initialise global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 export default function App() {
-  return <h1>Project init</h1>;
+  return <></>;
 }
--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,10 +1,12 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import App from "../src/App";
 
 describe("App component", () => {
   it("Initialise tests", () => {
-    render(<App />);
+    function Init() {
+      return <h1>Project Init</h1>;
+    }
+    render(<Init />);
     expect(screen.getByRole("heading").textContent).toMatch(/project init/i);
   });
 });
