5f707797089fe9cb1afc6ba67dac851566541f0b
SammySparrow <samstevens.dev@gmail.com>
2026-02-23 15:20:28 +1100

    Style Shop components error and loading messages

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,14 +1,25 @@
-import { useOutletContext } from "react-router";
+import { Link, useOutletContext } from "react-router";
 import LoadingCircle from "../../components/LoadingWheel/LoadingCircle";
+import styles from "./Shop.module.css";
+import Button from "../../components/Button/Button";
 
 export default function Shop() {
   const { loading, error, shopItems } = useOutletContext();
+
   if (loading)
     return (
-      <>
+      <div className={styles.message}>
         <LoadingCircle />
         <p>Loading</p>
-      </>
+      </div>
     );
-  if (error) return <p>An error was encountered</p>;
+  if (error)
+    return (
+      <div className={styles.message}>
+        <p>An error has occured</p>
+        <Link to="/">
+          <Button type="secondary" label="Return home" />
+        </Link>
+      </div>
+    );
 }
