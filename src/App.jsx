da666c40c5b14da7b1dad19b3a3c034a7b999f1f
SammySparrow <samstevens.dev@gmail.com>
2026-02-23 14:08:34 +1100

    Create helper to fetch from FakeStore API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { Outlet } from "react-router";
+import { useState } from "react";
+import { useStoreData } from "./Logic";
 import Nav from "./components/Nav/Nav";
 
 export default function App() {
+  const { shopItems, error, loading } = useStoreData();
+  const [cartList, setCartList] = useState([]);
+
   return (
     <>
-      <Nav />
-      <Outlet />
+      <Nav count={cartList.length} />
+      <Outlet
+        context={{
+          cart: cartList,
+          addToCart: setCartList,
+          shopItems: shopItems,
+          error: error,
+          loading: loading,
+        }}
+      />
     </>
   );
 }
---
+++ src/Logic.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export function useStoreData() {
+  const [shopItems, setShopItems] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((r) => {
+        if (r.status === 400) throw new Error("Server error");
+        return r.json();
+      })
+      .then((r) => {
+        setShopItems(r);
+        console.log(r);
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { shopItems, error, loading };
+}
