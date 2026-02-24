4b94253755cd8516f5cff40bbf74ceb9bf278662
SammySparrow <samstevens.dev@gmail.com>
2026-02-25 08:36:20 +1100

    Display related items inside Item page

--- src/pages/Item/Item.jsx
+++ src/pages/Item/Item.jsx
@@ -1,23 +1,41 @@
 import Button from "../../components/Button/Button";
 import QuantityInput from "../../components/QuantityInput/QuantityInput";
 import StarDisplay from "../../components/StarDisplay/StarDisplay";
+import { Link } from "react-router";
+import styles from "./Item.module.css";
 
-export default function Item({ item }) {
+export default function Item({ item, array }) {
+  const related = array.filter(
+    (arrItem) =>
+      item.category === arrItem.category && item.title !== arrItem.title,
+  );
   return (
-    <section>
-      <div>
+    <section className={styles.wrapper}>
+      <Link className={styles.back} to="/shop/browse">
+        &larr; Back to shop
+      </Link>
+      <div className={styles.imgwrapper}>
         <img src={item.image} alt="" />
       </div>
-      <div>
+      <div className={styles.textwrapper}>
         <h3>{item.title}</h3>
-        <div>
+        <div className={styles.row}>
           <StarDisplay rating={item.rating.rate} count={item.rating.count} />
-          <p>{`$${item.price}`}</p>
+          <p className={styles.price}>{`$${item.price}`}</p>
         </div>
         <p>{item.description}</p>
-        <div>
-          <QuantityInput />
-          <Button label="Add to cart" />
+
+        <QuantityInput />
+        <Button label="Add to cart" />
+      </div>
+      <div className={styles.more}>
+        <p>More like this:</p>
+        <div className={styles.related}>
+          {related.map((rel) => (
+            <Link to={`/shop/${rel.id}`}>
+              <img className={styles.preview} src={rel.image} alt="" />
+            </Link>
+          ))}
         </div>
       </div>
     </section>
