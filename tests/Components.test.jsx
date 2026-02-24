dd6c4c419004eb2415e49971853371992ea3c73a
SammySparrow <samstevens.dev@gmail.com>
2026-02-25 10:46:01 +1100

    Write tests for quantity control

--- tests/Components.test.jsx
+++ tests/Components.test.jsx
@@ -1,8 +1,11 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { createMemoryRouter, RouterProvider, Outlet } from "react-router";
+import userEvent from "@testing-library/user-event";
 import Cart from "../src/components/Cart/Cart";
 import ItemCard from "../src/components/ItemCard/ItemCard";
+import QuantityInput from "../src/components/QuantityInput/QuantityInput";
+import { useState } from "react";
 
 describe("Components", () => {
   describe("Cart component", () => {
@@ -30,6 +33,39 @@ describe("Components", () => {
     it("Correctly displays price with dollar sign", () => {
       render(<RouterProvider router={mockRouter} />);
       expect(screen.getByText("$20")).toBeInTheDocument();
+    });
+  });
+
+  describe("Quantity component", () => {
+    const MockParent = vi.fn(() => {
+      const [value, setValue] = useState(5);
+      const increment = vi.fn(() => setValue(value + 1));
+      const decrement = vi.fn(() => setValue(value - 1));
+      return (
+        <QuantityInput
+          value={value}
+          increment={increment}
+          decrement={decrement}
+        />
+      );
+    });
+    it("Displays default value", () => {
+      render(<MockParent />);
+      expect(screen.getByDisplayValue("5")).toBeInTheDocument();
+    });
+    it("+ button increments value", async () => {
+      render(<MockParent />);
+      const user = userEvent.setup();
+      const link = screen.getByRole("button", { name: "+" });
+      await user.click(link);
+      expect(screen.getByDisplayValue("6")).toBeInTheDocument();
+    });
+    it("- button decrements the value", async () => {
+      render(<MockParent />);
+      const user = userEvent.setup();
+      const link = screen.getByRole("button", { name: "-" });
+      await user.click(link);
+      expect(screen.getByDisplayValue("4")).toBeInTheDocument();
     });
   });
 });
