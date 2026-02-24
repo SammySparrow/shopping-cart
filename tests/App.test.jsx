53db6cd855731b2451fe9131b98226da4a280e75
SammySparrow <samstevens.dev@gmail.com>
2026-02-24 16:38:38 +1100

    Move shop page tests into new test file

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,39 +1,3 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { createMemoryRouter, Outlet, RouterProvider } from "react-router";
-import Shop from "../src/pages/Shop/Shop";
-
-describe("Shop page tests", () => {
-  it("Displays a loading page when `loading` is true", () => {
-    function MockParent() {
-      const loading = true;
-      return <Outlet context={{ loading }} />;
-    }
-    const shopRoute = createMemoryRouter([
-      {
-        path: "/",
-        element: <MockParent />,
-        children: [{ index: true, element: <Shop /> }],
-      },
-    ]);
-    render(<RouterProvider router={shopRoute} />);
-    expect(screen.getByRole("paragraph").textContent).toMatch(/loading/i);
-  });
-
-  it("Displays an error message when `error` isn't null", () => {
-    function MockParent() {
-      const loading = false;
-      const error = "bada bing";
-      return <Outlet context={{ loading, error }} />;
-    }
-    const shopRoute = createMemoryRouter([
-      {
-        path: "/",
-        element: <MockParent />,
-        children: [{ index: true, element: <Shop /> }],
-      },
-    ]);
-    render(<RouterProvider router={shopRoute} />);
-    expect(screen.getByRole("paragraph").textContent).toMatch(/error/i);
-  });
-});
